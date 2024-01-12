91bf74f90a31ab613cd5ffa14479047b87a932a8
UneGrandChevr <129595748+UneGrandChevr@users.noreply.github.com>
2024-01-12 14:45:44 +0100

    c'est bon

--- back/commands/musique.py
+++ back/commands/musique.py
@@ -1,11 +1,15 @@
 from utils.TextToSpeech import sayInstruction
 import re
+import threading 
+from tkinter import *
+import pygame
 
 def command():
     sayInstruction("Bien sûr, quel musique voulez vous jouer?")
 
 def specificity(param):
-    playMusic(param)
+    thread = threading.Thread(target=playMusic, args=(param,))
+    thread.start()
 
 def specificityName():
     return "musique"
@@ -13,4 +17,6 @@ def trigger(pText):
     return ((re.search("joue",pText)) or (re.search("jou",pText) or (re.search("jous",pText))or (re.search("musique",pText)))) and re.search("chançon", pText)
 
 def playMusic(param):
-    print(param)
+    pygame.mixer.init()
+    pygame.mixer.music.load(r"D:/a/flowbot/flowbot/file/music/"+param.lower()+".mp3")
+    pygame.mixer.music.play()
