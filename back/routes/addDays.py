87f18ba8975ad439bbc949848851d18d2c428dc3
zedroff <aman.ghazanfar@edu.esiee.fr>
2024-06-20 19:17:16 +0200

    most random push ever

--- back/routes/addDays.py
+++ back/routes/addDays.py
@@ -14,6 +14,18 @@ def add_days():
             # Générer un UUID
             uuid_value = str(uuid.uuid4())
             cur = con.cursor()
+            # Vérifier si un jour avec le même nom existe déjà
+            cur.execute("SELECT * FROM days WHERE name=? AND day=? AND f=? AND t=?", (data['name'], data['day'], data['f'], data['t']))
+            day = cur.fetchone()
+            if day:
+                return jsonify({'result': 'already_name'})
+            # Vérifier s'il existe déjà un jour avec les mêmes dates
+            cur.execute("SELECT * FROM days WHERE day=? AND f=? AND t=?", (data['day'], data['f'], data['t']))
+            day = cur.fetchone()
+            if day:
+                return jsonify({'result': 'already_date'})
+            
+            cur = con.cursor()
             cur.execute("INSERT INTO days (id, name, day, f, t) VALUES (?, ?, ?, ?, ?)", (uuid_value, data['name'], data['day'], data['f'], data['t']))
             con.commit()
             cur.close()
